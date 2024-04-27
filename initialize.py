2e148c98a1c4eed35e9cb077ee5845e65751f468
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-04-27 15:06:22 +0200

    Added logging to initialize

--- initialize.py
+++ initialize.py
@@ -19,16 +19,22 @@ def main(args):
     _, _, _, ds_info = build_dataset(args)
     for seed in range(args.num_seeds):
         initial_labeled_pool = random.sample(range(ds_info['n_samples']), k=args.al.initial_pool_size)
-        path = os.path.join(args.path.initial_pool_dir, args.dataset+'_'+str(args.al.initial_pool_size)+'_'+str(seed+1)+'.json')
-        if not os.path.exists(path):
-            with open(path, 'w') as f:
+        p_path = os.path.join(args.path.initial_pool_dir, args.dataset+'_'+str(args.al.initial_pool_size)+'_'+str(seed+1)+'.json')
+        if not os.path.exists(p_path):
+            logging.info(f"Created new pool at {p_path}!")
+            with open(p_path, 'w') as f:
                 json.dump(initial_labeled_pool, f)
+        else:
+            logging.info(f"There is already a pool at {p_path}!")
 
         model, _, _, _, _, _, _ = build_model(args=args, ds_info=ds_info)
-        path = os.path.join(args.path.model_dir, args.model.name+'_'+str(seed+1)+'.pth')
-        if not os.path.exists(path):
+        m_path = os.path.join(args.path.model_dir, args.model.name+'_'+str(seed+1)+'.pth')
+        if not os.path.exists(m_path):
+            logging.info(f"Created new model at {m_path}!")
             state_dict = model.state_dict()
-            torch.save(state_dict,path)
+            torch.save(state_dict,m_path)
+        else:
+            logging.info(f"There is already a model at {m_path}!")
 
 
 if __name__ == '__main__':
