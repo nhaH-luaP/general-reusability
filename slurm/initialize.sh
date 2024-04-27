fd5de63be36b5e20452a4af3aace65d09438a58f
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-04-27 15:07:22 +0200

    updated initialize slurm script

--- slurm/initialize.sh
+++ slurm/initialize.sh
@@ -5,19 +5,19 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 #SBATCH --mem=128GB
-#SBATCH --output=/mnt/stud/work/phahn/SDAL/logs/alc/initializing_%j.out
+#SBATCH --output=/mnt/stud/work/phahn/SDAL/logs/dal/initializing_%j.out
 date;hostname;pwd
 source /mnt/stud/work/phahn/venvs/sdal/bin/activate
 cd /mnt/stud/work/phahn/SDAL/SDAL
 
 dataset_path=/mnt/stud/work/phahn/datasets/
-pool_dir=/mnt/stud/work/phahn/BA/storage/initial_pools/
-model_dir=/mnt/stud/work/phahn/BA/storage/pretrained_models/
+pool_dir=/mnt/stud/work/phahn/SDAL/storage/initial_pools/
+model_dir=/mnt/stud/work/phahn/SDAL/storage/pretrained_models/
 
 python -u initialize.py \
         path.initial_pool_dir=$pool_dir \
         path.data_dir=$dataset_path \
         path.model_dir=$model_dir \
         dataset=cifar10 \
-	alc.initial_pool_size=250 \
+	alc.initial_pool_size=300 \
         model=wideresnet282
