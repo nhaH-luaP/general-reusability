bd37675add3cb46f657e1c28721ff5cb8650e37f
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-05-15 10:18:02 +0200

    Updated fut eval slurm

--- slurm/evaluate_pool_fut.sh
+++ slurm/evaluate_pool_fut.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=SDAL_DAL
+#SBATCH --job-name=SDAL_FUT
 #SBATCH --partition=main
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 #SBATCH --mem=128GB
-#SBATCH --array=0-59%4
-#SBATCH --output=/mnt/stud/work/phahn/SDAL/logs/eval/%A_%a_%x.out
+#SBATCH --array=0-179%4
+#SBATCH --output=/mnt/stud/work/phahn/SDAL/logs/eval/fut/%A_%a_%x.out
 
 # Active Environment, change to directory and print certain infos
 date;hostname;pwd
@@ -14,21 +14,27 @@ source /mnt/stud/work/phahn/venvs/sdal/bin/activate
 cd /mnt/stud/work/phahn/SDAL/SDAL
 
 # Create tupel of variables
-models=(resnet10 resnet18 resnet34 resnet50)
+models=(resnet6 resnet10 resnet18)
 queries=(random margin coreset badge typiclust)
+ssl=(False True)
+pre=(False True)
 random_seeds=(1 2 3)
 
 # Get the current task index from the job array and select instances of variables based on it
 index=$SLURM_ARRAY_TASK_ID
-query_model=${models[$index % 4]}
-query=${queries[$index / 4 % 5]}
-seed=${random_seeds[$index / 20]}
-eval_model=wideresnet2810
+query_model=${models[$index % 3]}
+s=${ssl[$index / 3 % 2]}
+p=${pre[$index / 6 % 2]}
+query=${queries[$index / 12 % 5]}
+eval_model=wideresnet282
+eval_s=False
+eval_p=False
+seed=${random_seeds[$index / 60]}
 
 # Predefine certain paths
 dataset_path=/mnt/stud/work/phahn/datasets/
-final_pool_dir=/mnt/stud/work/phahn/SDAL/storage/final_pools/${query_model}/${query}/seed_${seed}/
-output_dir=/mnt/stud/work/phahn/SDAL/output/eval_fut/${eval_model}/${query_model}/${query}/seed_${seed}/
+final_pool_dir=/mnt/stud/work/phahn/SDAL/storage/final_pools/${model}/${query}/ssl_${s}/pre_${p}/seed_${seed}/
+output_dir=/mnt/stud/work/phahn/SDAL/output/eval_fut/${query_model}/${query}/ssl_${s}/pre_${p}/seed_${seed}/${eval_model}/ssl_${eval_s}/pre_${eval_p}/
 model_dir=/mnt/stud/work/phahn/SDAL/storage/initial_weights/
 
 # Run experiment
@@ -38,4 +44,6 @@ python -u evaluate_pool.py \
         path.final_pool_dir=$final_pool_dir \
         path.model_dir=$model_dir \
         random_seed=$seed \
-        model=$eval_model
+        model=$eval_model \
+        ssl.use=$eval_s \
+        pretrain.use=$eval_p \
