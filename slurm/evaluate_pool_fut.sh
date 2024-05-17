4a00f740bfc47db272a552558c3fcfe594ef99c2
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-05-17 10:27:33 +0200

    Created new slurm file for future models

--- slurm/evaluate_pool_fut.sh
+++ slurm/evaluate_pool_fut.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 #SBATCH --mem=128GB
-#SBATCH --array=0-179%4
+#SBATCH --array=120-479%4
 #SBATCH --output=/mnt/stud/work/phahn/SDAL/logs/eval/fut/%A_%a_%x.out
 
 # Active Environment, change to directory and print certain infos
@@ -15,10 +15,11 @@ cd /mnt/stud/work/phahn/SDAL/SDAL
 
 # Create tupel of variables
 models=(resnet6 resnet10 resnet18)
+eval_models=(wideresnet282 wideresnet2810)
 queries=(random margin coreset badge typiclust)
 ssl=(False True)
 pre=(False True)
-random_seeds=(1 2 3)
+random_seeds=(1)
 
 # Get the current task index from the job array and select instances of variables based on it
 index=$SLURM_ARRAY_TASK_ID
@@ -26,14 +27,14 @@ query_model=${models[$index % 3]}
 s=${ssl[$index / 3 % 2]}
 p=${pre[$index / 6 % 2]}
 query=${queries[$index / 12 % 5]}
-eval_model=wideresnet282
-eval_s=False
-eval_p=False
-seed=${random_seeds[$index / 60]}
+eval_model=${eval_models[$index / 60 % 2]}
+eval_s=${ssl[$index / 120 % 2]}
+eval_p=${pre[$index / 240 % 2]}
+seed=${random_seeds[$index / 480 % 3]}
 
 # Predefine certain paths
 dataset_path=/mnt/stud/work/phahn/datasets/
-final_pool_dir=/mnt/stud/work/phahn/SDAL/storage/final_pools/${model}/${query}/ssl_${s}/pre_${p}/seed_${seed}/
+final_pool_dir=/mnt/stud/work/phahn/SDAL/storage/final_pools/${query_model}/${query}/ssl_${s}/pre_${p}/seed_${seed}/
 output_dir=/mnt/stud/work/phahn/SDAL/output/eval_fut/${query_model}/${query}/ssl_${s}/pre_${p}/seed_${seed}/${eval_model}/ssl_${eval_s}/pre_${eval_p}/
 model_dir=/mnt/stud/work/phahn/SDAL/storage/initial_weights/
 
